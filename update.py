6087c024730b9d5e76cfb0770144a82c708d22a6
shivaallala <76643874+shivaallala@users.noreply.github.com>
2022-08-11 13:05:00 -0400

    Update update.py

--- update.py
+++ update.py
@@ -1,3 +1,5 @@
+import os.path
+
 class Employee: 
     employeeList = list()
     def __init__(self, empNo, empName, empDep, empSal):
@@ -5,7 +7,7 @@ class Employee:
      
     def addNewEmployee(self):   
         Employee.employeeList.append(self)
-    
+        
     def getEmpList(self):
         return Employee.employeeList
     
@@ -21,6 +23,12 @@ class Employee:
                 emp.empNo, emp.empName, emp.empDep, emp.empSal = empNo, empName, empDep, empSal   
                 return True
         return False
+    
+    def removeEmpByID(self, empNo):
+        for emp in Employee.employeeList: 
+            if(emp.empNo == empNo):
+                Employee.employeeList.remove
+              
     
     def setEmpNo(self, empNo):
         self.empNo = empNo
@@ -46,16 +54,42 @@ class Employee:
 
 choice = 1
 employee = Employee(0," ", " ", 0.0)
-while choice >= 1 and choice <= 4:
-    print("\n\n1. Add New Employee\n2. Get all Employee List\n3. Get Employee ID number\n4. Update Employee By ID \n\n")
+file_exists = os.path.exists('database.txt')
+if file_exists == False:
+    f = open("database.txt", "x")
+else:
+    print("File exists")
+f = open("database.txt", "a")
+
+while choice >= 1 and choice <= 5:
+    print("\n\n1. Add New Employee\n2. Get all Employee List\n3. Get Employee ID number\n4. Update Employee By ID \n5. Remove Employee by ID\n\n")
     choice = int(input("Enter your Choice: "))
     if(choice == 1 ):
-        empNo = int(input("Enter Your Employee ID number: "))
+        while True:
+            try:
+                empNo = int(input("Enter Your Employee ID number: "))
+                break
+            except ValueError:
+                print("Please enter an ID")
+         
         empName = input("Enter Employee Name: ")
         empDep = input("Enter Employee Department: ")
-        empSal = float(input("Enter Employee Salary: "))
-        emp = Employee(empNo, empName, empDep, empSal )
+        while True:
+            try:
+                empSal = float(input("Enter Employee Salary: "))
+                break
+            except ValueError:
+                print(" please enter a valid Salary")
+        emp = Employee(empNo, empName, empDep, empSal)
         emp.addNewEmployee()
+        f.writelines(f"{(empNo)}\n")
+        f.writelines(f"{str(empName)}\n")
+        f.writelines(f"{str(empDep)}\n")
+        f.writelines(f"{(empSal)}\n")
+        f.writelines("====================")
+        f.writelines("\n")
+        
+        
     
     elif(choice == 2 ):
         print("\n")
@@ -63,19 +97,37 @@ while choice >= 1 and choice <= 4:
             print(emp)
             
     elif(choice == 3):
-        empNo = int(input("Enter Employee Id:  "))
+        while True:
+            try:
+                empNo = int(input("Enter Employee Id:  "))
+                break
+            except ValueError:
+                print("please enter a valid ID ")
         emp = employee.getEmpById(empNo)
         if(emp == False):
             print("\nSorry...! Employee not found for id: " , empNo)
         else:
             print(emp)
     elif(choice == 4):
-        empNo = int(input("Enter Your Employee ID number: "))
+        while True:
+            try:
+                empNo = int(input("Enter Your Employee ID number: "))
+                break
+            except ValueError:
+                print("please enter a valid Id")
         empName = input("Enter Employee Name: ")
         empDep = input("Enter Employee Department: ")
-        empSal = float(input("Enter Employee Salary: "))
+        while True:
+            try:
+                empSal = float(input("Enter Employee Salary: "))
+                break
+            except ValueError:
+                print("please enter a valid salary")
         emp = employee.updateEmpById(empNo, empName, empDep, empSal)
         if(emp == False):
             print("\nSorry..! Update Failed. Employee Not Found ID: ", empNo)
         else:
             print("SuccessFully Employee update Id: ", empNo)
+    elif(choice == 5):
+        empID = int(input("enter an employee Id to remove:"))
+        employee.removeEmpByID(empID)
